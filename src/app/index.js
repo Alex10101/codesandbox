4471d97a8de27caf0ea3a7a75caafb362a3c842a
Aliaksei Senchanka <Aliaksei_Senchanka@epam.com>
2020-01-29 20:01:35 +0300

    Base

---
+++ src/app/index.js
@@ -0,0 +1,4 @@
+import angular from "angular";
+import phonesComponent from "./components/phones/phones.component";
+
+angular.module("app", [phonesComponent]);
---
+++ webpack.config.js
@@ -0,0 +1,116 @@
+const webpack = require('webpack');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+
+const path = require('path');
+
+const sourcePath = path.resolve(__dirname, 'src');
+const distPath = path.resolve(__dirname, 'dist');
+
+module.exports = (env, argv) => {
+  const isProd = argv.mode === 'production';
+
+  const plugins = [
+    new HtmlWebPackPlugin({
+      template: sourcePath + '/index.html'
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].[contenthash:4].css',
+      chunkFilename: '[id].[contenthash:4].css'
+    })
+  ];
+
+  if (isProd) {
+    plugins.push(
+      new webpack.NormalModuleReplacementPlugin(
+        /\/environments\/environment\.ts/,  `${sourcePath}/environments/environment.prod.ts`
+      ),
+      new UglifyJsPlugin({ sourceMap: true })
+    );
+  } else {
+    plugins.push(new webpack.NamedModulesPlugin(), new webpack.HotModuleReplacementPlugin());
+  }
+
+  const config = {
+    entry: {
+      app: sourcePath + '/app/index.js',
+    },
+    output: {
+      path: distPath,
+      filename: '[name].bundle.[hash:4].js',
+    },
+    module: {
+      rules: [
+        {
+          test: /\.html$/,
+          loader: 'html-loader',
+          options: { minimize: true }
+        },
+        {
+          test: /\.css$/,
+          use: [MiniCssExtractPlugin.loader, 'css-loader', 'resolve-url-loader']
+        },
+        {
+          test: /\.scss$/,
+          use: [MiniCssExtractPlugin.loader, 'css-loader', 'resolve-url-loader', 'sass-loader',
+            {
+              loader: 'sass-resources-loader',
+              options: {
+                resources: [
+                  './src/styles_global/_colors.scss',
+                  './src/styles_global/_mixins.scss',
+                  './src/styles_global/_utils.scss',
+                ]
+              }
+            }
+          ]
+        },
+        {
+          test: /\.(gif|png|jpe?g|svg)$/i,
+          loader: 'file-loader',
+          options: {
+            name: 'images/[name].[ext]'
+          }
+        },
+        {
+          test: /\.(eot|ttf|woff|woff2)$/,
+          loader: 'file-loader',
+          options: {
+            name: 'fonts/[name].[ext]'
+          }
+        }
+      ],
+    },
+    resolve: {
+      extensions: ['.js', '.ts'],
+      modules: [
+        path.resolve(__dirname, 'node_modules'),
+        sourcePath
+      ]
+    },
+    plugins,
+    optimization: {
+      splitChunks: {
+        cacheGroups: {
+          commons: {
+            test: /[\\/]node_modules[\\/]/,
+            name: 'vendors',
+            chunks: 'all'
+          }
+        }
+      }
+    },
+    // devtool: 'eval-source-map',
+    devServer: {
+      contentBase: distPath,
+      hot: true
+    }
+  };
+
+  if (!isProd) {
+    config.devtool = 'source-map';
+  }
+
+  return config;
+};
